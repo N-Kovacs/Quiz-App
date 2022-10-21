047ae6ee271b5d1b819d51a1a0bd21810101f17c
colspen <spencerlewiscole@gmail.com>
2022-10-21 22:22:09 +0000

    small spacing in schema 04

--- db/schema/04_quiz_results.sql
+++ db/schema/04_quiz_results.sql
@@ -1,12 +1,13 @@
 -- Drop and recreate multiple choice questions
 
 DROP TABLE IF EXISTS quiz_results CASCADE;
+
 CREATE TABLE quiz_results (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
   score SMALLINT NOT NULL,
-  max_score SMALLINT NOT NULL,
+  max_score SMALLINT NOT NULL
 );
 
 
