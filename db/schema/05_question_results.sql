6682f6a50a61fbe655203d7e6066dcb418b47550
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-10-24 19:44:45 +0000

    database schema

---
+++ db/schema/05_question_results.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate multiple choice questions
+DROP TABLE IF EXISTS question_results CASCADE;
+
+CREATE TABLE question_results (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  quiz_results_id INTEGER REFERENCES quiz_results(id) ON DELETE CASCADE,
+  question_id INTEGER REFERENCES questions_multiple_choice(id) ON DELETE CASCADE,
+  correct BOOLEAN NOT NULL,
+);
