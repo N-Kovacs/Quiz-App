457a27c636b833a9b593d7db6d346e244481d3c5
colspen <spencerlewiscole@gmail.com>
2022-10-25 21:40:47 +0000

    answers scramble, elementshide and show correctly in quizzes/id

--- public/scripts/quizzes_attempt.js
+++ public/scripts/quizzes_attempt.js
@@ -7,21 +7,19 @@ let question_results = []; //fill with answers
 //
 
 $(() => {
-
   loadQuestions();
   console.log("* INSIDE doc ready()");
-
 });
 
 const loadQuestions = () => {
   //.get.then(don't need ajax params object)
   $.get("/api/questions")
     .then(data => {
-
       questions = data; // Questions is an ARRAY
+
       showCurrentQuestion();
 
-      console.log("* INSIDE loadQuestion()");
+      console.log("* INSIDE loadQuestion()", data);
     });
 };
 
@@ -29,33 +27,47 @@ const showCurrentQuestion = () => {
   const question = questions[currentQuestionIndex];
   console.log("* INSIDE showCurrentQuestion()", question);
 
-  const renderQuiz = makeQuiz(question);
+  const randomAnswers = scrambleAnswers(question);
+  const renderQuiz = makeQuiz(question, randomAnswers);
   $('#quiz-attempt').empty(renderQuiz);
   $('#quiz-attempt').append(renderQuiz);
   $('#next-question').hide();
   $('#next-question').on('click', getNextQuestion);
 
   $('.answers').on('click', (event) => {
-    // $('.quiz-dyn-buttons > h4').html('Yes! You are correct!');
-    // $('.quiz-dyn-buttons > h4').html('Sorry. That is incorrect!');
+    const btnVal = event.target.value;
+    const $buttonTxt = $(`button[value="${btnVal}"]`).text();
+
+    console.log("* * * * * * * * * * * * * ", $buttonTxt);
+    console.log("* * * * * * * * * * * * * ", question.correct_answer);
+
+    console.log("TEST", scrambleAnswers(question));
+
+    if ($buttonTxt === question.correct_answer) {
+      $('.quiz-dyn-buttons > h4').html('Yes! You are correct!');
+    }
+    $('.quiz-dyn-buttons > h4').html('Sorry. That is incorrect!');
     $('#next-question').show();
-
-
   });
-
-
-
   // $('.quiz-header').css('background-image', question.image_url);
-
 };
 
 const getNextQuestion = () => {
+  console.log("* INSIDE getNextQuestion()");
   currentQuestionIndex++;
   if (currentQuestionIndex > questions.length - 1) return;
   showCurrentQuestion();
 };
 
-const makeQuiz = (quiz) => {
+const scrambleAnswers = (answers) => {
+  const answerArr = [
+    answers.incorrect_answer_one, answers.incorrect_answer_two, answers.incorrect_answer_three, answers.correct_answer
+  ];
+  const randomArr = answerArr.sort(() => Math.random() - 0.5);
+  return randomArr;
+};
+
+const makeQuiz = (quiz, randomAnswers) => {
   let $quizStructure =
     $(`
   <section class="make-quiz">
@@ -63,7 +75,6 @@ const makeQuiz = (quiz) => {
           <p>Question ${quiz.id}</p>
           <h3 id="question">${quiz.question}</h3>
         </div>
-
 
         <div class="quiz-dyn-buttons">
             <h4>&nbsp</h4>
@@ -73,34 +84,19 @@ const makeQuiz = (quiz) => {
         </div>
 
         <div class="answers">
-          <button value="1">${quiz.incorrect_answer_one}
+          <button value="1">${randomAnswers[0]}
           </button>
-          <button value="2">${quiz.incorrect_answer_two}
+          <button value="2">${randomAnswers[1]}
           </button>
-          <button value="3">${quiz.incorrect_answer_three}
+          <button value="3">${randomAnswers[2]}
           </button>
-          <button value="4">${quiz.correct_answer}
+          <button value="4">${randomAnswers[3]}
           </button>
         </div>
       </section>
   `);
   return $quizStructure;
 };
-
-//target answer button, when click, read id.
-
-/* <div class="true-false">
-<h4>You are correct!</h4>
-<h4>Nope, incorrect!</h4>
-</div>
-<button id="next-question">Next</button> */
-
-//$(".answers button").click(event => (console.log(event.target.value)));
-
-
-
-//
-// do not scramble button scramble ANSWER
 
 /* <div>
   <img src="" />
