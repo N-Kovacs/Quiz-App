d4d0eb8b7dfe70383076a5efc52034fbbd0793f2
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-10-28 02:49:45 +0000

    fix results edgecase

--- db/queries/quiz_results.js
+++ db/queries/quiz_results.js
@@ -16,8 +16,9 @@ const getAttemptsAverageScoreFromQuizID = (id) => {
   return db.query(`SELECT COUNT(quiz_results.*) as global_attempts, AVG(score) as average_score, max_score
   FROM quiz_results
   JOIN quizzes ON quiz_results.quiz_id = quizzes.id
-  GROUP BY quizzes.id, score, max_score
-  HAVING quizzes.id = $1;`, [id])
+  WHERE quizzes.id = $1
+  GROUP BY quizzes.id, max_score
+  `, [id])
     .then(data => {
       return data.rows;
     });
